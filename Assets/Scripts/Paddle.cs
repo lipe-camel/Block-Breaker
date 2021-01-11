a3d93913de232eefef017b864bc169d025fa2263
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-11 17:33:13 -0300

    BB_64

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float horizontalScreenUnits = 16f;
+    [SerializeField] float minxPos = 1f;
+    [SerializeField] float maxPos = 15f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float getMouseXPos = Input.mousePosition.x / Screen.width * horizontalScreenUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(getMouseXPos, minxPos, maxPos);
+        transform.position = paddlePos;
+    }
+}
