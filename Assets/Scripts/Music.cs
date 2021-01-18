55ec91aada47af1411f4b73aab0020e4bc16fb99
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-18 15:58:47 -0300

    added music and one more bounce sound

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private void Awake() //singleton pattern
+    {
+        int gameStatusCount = FindObjectsOfType<Music>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
