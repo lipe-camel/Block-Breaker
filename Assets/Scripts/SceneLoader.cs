ae3bdc2984fc662a1104f0f2eeeaadecb0d07e5d
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-18 21:09:35 -0300

    Added levels and UI

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] AudioClip buttonPressSound;
+    public void PlayButtonSound()
+    {
+        AudioSource.PlayClipAtPoint(buttonPressSound, Camera.main.transform.position);
+    }
+
+
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -21,12 +28,21 @@ public class SceneLoader : MonoBehaviour
     public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void EraseScore()
+    {
         FindObjectOfType<ScoreSystem>().EraseScore();
     }
 
     public void LoadLoseScene()
     {
-        SceneManager.LoadScene("Lose Screen");
+        SceneManager.LoadScene("End Screen");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 
     public void QuitGame()
